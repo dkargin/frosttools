6de9c76c47b3b2c3c8c5844591e71c3709038ff7
vrobot <vrobot@pcmain.(none)>
2013-02-26 10:00:39 +0400

    more fix

--- FrostToolsConfig.cmake
+++ FrostToolsConfig.cmake
@@ -6,7 +6,7 @@
 # FROSTTOOLS_DEFINITIONS
 
 find_path(FROSTTOOLS_INCLUDE_DIR frosttools/frosttools.h HINTS /usr/include /usr/local/include ${FrostTools_DIR}/include)
-find_library(FROSTTOOLS_LIBRARY_NET NAMES frosttools HINTS /usr/lib /usr/local/lib ${FrostTools_DIR}/lib)
+find_library(FROSTTOOLS_LIBRARY_NET NAMES frosttools HINTS /usr/lib/frosttools/ /usr/local/lib/frosttools/ ${FrostTools_DIR}/lib)
 
 if(FROSTTOOLS_INCLUDE_DIR)
 	message(STATUS "FrostTools found : ${FROSTTOOLS_INCLUDE_DIR}")
@@ -17,10 +17,10 @@ else(FROSTTOOLS_INCLUDE_DIR)
 endif(FROSTTOOLS_INCLUDE_DIR)
 
 if(FROSTTOOLS_LIBRARY_NET)
-	set(FROSTTOOLS_LIBRARIES ${FROSTTOOLS_LIBRARY_NET})
+	set(FROSTTOOLS_LIBRARIES ${FROSTTOOLS_LIBRARIES} ${FROSTTOOLS_LIBRARY_NET})
 	message(STATUS "FrostTools lib found : ${FROSTTOOLS_LIBRARY_NET}")	
 else(FROSTTOOLS_LIBRARY_NET)
-	message(STATUS "NodeNet library not found")
+	message(STATUS "FrostTools library not found")
 	set(FROSTTOOLS_FOUND FALSE)
 endif(FROSTTOOLS_LIBRARY_NET)
 
